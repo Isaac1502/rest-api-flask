dda917ed40974d3b91fc51bedbe2085f4ed93cf1
itadina <itadina@growthaccelerationpartners.com>
2023-07-30 22:43:19 -0500

    add item description

--- schemas.py
+++ schemas.py
@@ -4,6 +4,7 @@ from marshmallow import Schema, fields
 class PlainItemSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
+    description = fields.Str()
     price = fields.Float(required=True)
 
 
